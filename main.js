8e788f30503d4537f8b8e3b3957f116f1532112c
Victor Sriqui <victorsriqui@gmail.com>
2023-04-21 22:13:01 -0600

    feat: Random comp choice

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ var computerName = document.querySelector(".computer__name");
 var computerWins = document.querySelector(".computer__wins");
 
 var bubbler = document.querySelectorAll('.center');
-
-
+var classicButton = document.getElementById("classic");
+var difficultButton = document.getElementById("difficult");
+var reset = document.querySelector(".user__reset");
+var centerPlay2 = document.querySelector(".center__play2");
 
 var classic = ['🪨', '📄', '✂️'];
 var difficult = ['🪨', '📄', '✂️', '🦎', '👽'];
@@ -18,21 +20,28 @@ computerPlayer = createPlayer('Computer', '💻', 0);
 updateUsers();
 }
 
+reset.onclick = function() {
+    (classicButton.style.display ="inline") && (difficultButton.style.display="inline");
+        console.log('hey')
+    reset.style.display = "none";    
+}
+
 bubbler.forEach(b => b.addEventListener('click', (e) => {
     
-    if  (e.target.id === 'classic'){
+    if (e.target.id === 'classic'){
         console.log('HOWDY DO')
         game = createGame(classic);
-        
+        (classicButton.style.display ="none") && (difficultButton.style.display="none");
+        reset.style.display = "inline";  
     } else if (e.target.id === 'difficult'){
         console.log('Nuh, Uhh')
         game = createGame(difficult);
-        
+        (classicButton.style.display ="none") && (difficultButton.style.display="none");
+        reset.style.display = "inline";
+        displayChoices()  
     }
     
   }))
-
-
 
 function createPlayer(name, token, wins) {
     return {
@@ -68,3 +77,24 @@ function hide(element) {
     element.classList.remove("hidden")
 }
 
+function computerChoice() {
+    var random = Math.floor(Math.random() * game.gameType.length) + 1
+    if (random === 1) {
+        game.player2.choice = '🪨'
+      }
+      if (random === 2) {
+        game.player2.choice = '📄'
+      }
+      if (random === 3) {
+        game.player2.choice = '✂️'
+      }
+      if (random === 4) {
+        game.player2.choice = '🦎'
+      }
+      if (random === 5) {
+        game.player2.choice = '👽'
+      }
+      return game
+    }
+
+  
