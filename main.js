e3f1289d60bda4a7985ab2d366d7edc167bd6ba7
Victor Sriqui <victorsriqui@gmail.com>
2023-04-22 23:01:55 -0600

    refactor: Cleaning up repeats into own functions, console logs, splitting up functions doing too many things,  etcetera

--- main.js
+++ main.js
@@ -6,15 +6,20 @@ var computerName = document.querySelector(".computer__name");
 var computerWins = document.querySelector(".computer__wins");
 
 var delegator = document.querySelectorAll('.center');
-var classicButton = document.getElementById("classic");
-var difficultButton = document.getElementById("difficult");
+// var classicButton = document.getElementById("classic");
+// var difficultButton = document.getElementById("difficult");
 var reset = document.querySelector(".user__reset");
 var centerPlay2 = document.querySelector(".center__play2");
 var centerPlay1 = document.querySelector(".center__play1");
 
+// Global Variables //
 var classic = ['🪨', '📄', '✂️'];
 var difficult = ['🪨', '📄', '✂️', '🦎', '👽'];
+var userPlayer;
+var computerPlayer;
+var game;
 
+// Event Listeners //
 onload = function() {
 userPlayer = createPlayer('Human', '👩🏻', 0);
 computerPlayer = createPlayer('Computer', '💻', 0);
@@ -27,32 +32,40 @@ reset.onclick = function() {
     gameModes();    
 }
 
-function gameModes(){
-    centerPlay1.innerHTML ='<button class ="play__button" id="classic">CLASSIC <br><br> 🪨 > ✂️ <br> 📄 > 🪨 <br> ✂️ > 📄 </button>';
-    centerPlay2.innerHTML = ' <button class ="play__button" id="difficult">DIFFICULT <br><br> 🪨 > ✂️ & 🦎 <br> 📄 > 🪨 & 👽 <br> ✂️ > 📄 & 🦎 <br> 🦎 > 📄 & 👽 <br> 👽 > ✂️ & 🪨 <br></button>';
-    reset.style.display = "none";
-}
-
 delegator.forEach(b => b.addEventListener('click', (e) => {
     if (e.target.id === 'classic'){
-        game = createGame(classic);
-        centerPlay2.innerHTML = '';
-        centerPlay1.innerHTML = '';
-        reset.style.display = "inline";
-        displayFighters();   
+        gameSetUp(classic);  
     } else if (e.target.id === 'difficult'){
-        game = createGame(difficult);
-        centerPlay2.innerHTML = '';
-        centerPlay1.innerHTML = '';
-        reset.style.display = "inline";
-        displayFighters();  
+        gameSetUp(difficult);
     } else if (e.target.id === '🪨' || e.target.id === '📄' || e.target.id === '✂️' || e.target.id === '🦎' || e.target.id === '👽') {
-        console.log('wwowwwooooh');
         game.player1.choice = e.target.id;
         aiChoice();
         decideWinner();
     }
 }))
+
+// Functions and Event Handlers //
+function gameSetUp(type) {
+    game = createGame(type);
+    clearBoard();
+    showChangeGame();
+    displayFighters(); 
+}
+
+function clearBoard() {
+  centerPlay2.innerHTML = '';
+  centerPlay1.innerHTML = '';
+}
+
+function showChangeGame() {
+  reset.style.display = "inline";
+}
+
+function gameModes(){
+  centerPlay1.innerHTML ='<button class ="play__button" id="classic">CLASSIC <br><br> 🪨 > ✂️ <br> 📄 > 🪨 <br> ✂️ > 📄 </button>';
+  centerPlay2.innerHTML = ' <button class ="play__button" id="difficult">DIFFICULT <br><br> 🪨 > ✂️ & 🦎 <br> 📄 > 🪨 & 👽 <br> ✂️ > 📄 & 🦎 <br> 🦎 > 📄 & 👽 <br> 👽 > ✂️ & 🪨 <br></button>';
+  reset.style.display = "none";
+}
 
 function createPlayer(name, token, wins) {
     return {
@@ -70,7 +83,6 @@ function createGame(gameType){
       draws: 0,
     };
 }
-    
 
 function updateUsers() {
     userToken.innerText = userPlayer.token
@@ -81,46 +93,25 @@ function updateUsers() {
     computerWins.innerText = `Wins: ${computerPlayer.wins}`
 }
 
-function hide(element) {
-    element.classList.add("hidden")
-  }
-  
-  function show(element) {
-    element.classList.remove("hidden")
-}
-
 function aiChoice() {
     var random = Math.floor(Math.random() * game.gameType.length) + 1;
     if (random === 1) {
         game.player2.choice = '🪨'
-      }
-      if (random === 2) {
+    } else if (random === 2) {
         game.player2.choice = '📄'
-      }
-      if (random === 3) {
+    } else if (random === 3) {
         game.player2.choice = '✂️'
-      }
-      if (random === 4) {
+    } else if (random === 4) {
         game.player2.choice = '🦎'
-      }
-      if (random === 5) {
+    } else if (random === 5) {
         game.player2.choice = '👽'
-      }
-      // return game
     }
-
-  
-// function display() {
-//     console.log("jibbajabba")
-    
-//     centerPlay2.innerHTML = `<button id='0'> ${game.gameType[0]} </button>`
-// }
+}
 
 function displayFighters() {
-    console.log("jibbajabba")
     for (i=0; i<game.gameType.length; i++) {
         if (game.gameType[i] === '🪨' || game.gameType[i] === '📄' || game.gameType[i] === '✂️') {
-    centerPlay1.innerHTML += `<button id='${game.gameType[i]}'> ${game.gameType[i]} </button>`
+            centerPlay1.innerHTML += `<button id='${game.gameType[i]}'> ${game.gameType[i]} </button>`
         }
         if (game.gameType[i] === '🦎' || game.gameType[i] === '👽') {
             centerPlay2.innerHTML += `<button id='${game.gameType[i]}'> ${game.gameType[i]} </button>`
@@ -129,49 +120,47 @@ function displayFighters() {
 }
 
 function decideWinner() {
-  if (game.player1.choice === game.player2.choice){
-    game.player1.recentResult = 'draw'
-    console.log('draw')
-  } else if (game.player1.choice === '🪨' && (game.player2.choice === '✂️' || game.player2.choice === '🦎')){
-    game.player1.recentResult = 'won'
-    console.log('winner')
-  } else if (game.player1.choice === '📄' && (game.player2.choice === '🪨' || game.player2.choice === '👽')){
-    game.player1.recentResult = 'won'
-    console.log('winner')
-  } else if (game.player1.choice === '✂️' && (game.player2.choice === '📄' || game.player2.choice === '🦎')){
-    game.player1.recentResult = 'won'
-    console.log('winner')
-  } else if (game.player1.choice === '🦎' && (game.player2.choice === '📄' || game.player2.choice === '👽')){
-    game.player1.recentResult = 'won'
-    console.log('winner')
-  } else if (game.player1.choice === '👽' && (game.player2.choice === '✂️' || game.player2.choice === '🪨')){
-    game.player1.recentResult = 'won'
-    console.log('winner')
-  } else {
-    game.player1.recentResult = 'lost'
-    console.log('loser')
-  }
-  counter()
-  resultMessage()
+    if (game.player1.choice === game.player2.choice){
+        game.player1.recentResult = 'draw'
+    } else if (game.player1.choice === '🪨' && (game.player2.choice === '✂️' || game.player2.choice === '🦎')){
+        game.player1.recentResult = 'won'
+    } else if (game.player1.choice === '📄' && (game.player2.choice === '🪨' || game.player2.choice === '👽')){
+        game.player1.recentResult = 'won'
+    } else if (game.player1.choice === '✂️' && (game.player2.choice === '📄' || game.player2.choice === '🦎')){
+        game.player1.recentResult = 'won'
+    } else if (game.player1.choice === '🦎' && (game.player2.choice === '📄' || game.player2.choice === '👽')){
+        game.player1.recentResult = 'won'
+    } else if (game.player1.choice === '👽' && (game.player2.choice === '✂️' || game.player2.choice === '🪨')){
+        game.player1.recentResult = 'won'
+    } else {
+        game.player1.recentResult = 'lost'
+    }
+    counter()
+    resultMessage()
 }
 
 function counter() {
-  if (game.player1.recentResult === 'won'){
-    game.player1.wins += 1;     
-  } else if (game.player1.recentResult === 'lost'){
-    game.player2.wins += 1;   
-  } else {
-    game.draws += 1;
-  }
-  updateUsers()
+    if (game.player1.recentResult === 'won'){
+        game.player1.wins += 1;     
+    } else if (game.player1.recentResult === 'lost'){
+        game.player2.wins += 1;   
+    } else {
+        game.draws += 1;
+    }
+    updateUsers()
 }
 
 function resultMessage (){
-  if (game.player1.recentResult === 'won'){ 
-    game.gameMessage = `${game.player1.token} ${game.player1.name} won this round! ${game.player1.token}`;
-  } else if (game.player1.recentResult === 'lost'){ 
-    game.gameMessage = `${game.player2.token} ${game.player2.name} won this round! ${game.player2.token}`;
-  } else {
-    game.gameMessage = `😭 It's a ${game.player1.recentResult} 😭`
-  }
+    if (game.player1.recentResult === 'won'){ 
+        game.gameMessage = `${game.player1.token} ${game.player1.name} won this round! ${game.player1.token}`;
+    } else if (game.player1.recentResult === 'lost'){ 
+        game.gameMessage = `${game.player2.token} ${game.player2.name} won this round! ${game.player2.token}`;
+    } else {
+        game.gameMessage = `😭 It's a ${game.player1.recentResult} 😭`
+    }
+    displayResults()
+}
+
+function displayResults() {
+  clearBoard()
 }
